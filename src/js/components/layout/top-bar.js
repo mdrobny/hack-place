78f23e983da6108fb412016441247eca5b1c8cdd
Mateusz Titz <mateusz.titz@schibsted.pl>
2015-07-28 15:51:05 +0200

    Add paddings

--- src/js/components/layout/top-bar.js
+++ src/js/components/layout/top-bar.js
@@ -10,15 +10,15 @@ var TopBar = React.createClass({
     render() {
         return (
             <div className="top-bar">
-                <div className="left-side">
+                <div className="top-bar-element">
                     <div className="logo">
                         <Link to="home"><img src="images/logo.png" alt="Enably.ng"/></Link>
                     </div>
                 </div>
-                <div>
+                <div className="upload-btn">
                     <Link to="upload" className="add-photo-link">Dodaj foto</Link>
                 </div>
-                <div className="right-side"></div>
+                <div className="top-bar-element"></div>
             </div>
         );
     }
--- src/js/components/place.js
+++ src/js/components/place.js
@@ -8,16 +8,26 @@ var PlaceView = React.createClass({
 
     render() {
         return (
-            <div className="place-wrapper">
-                <div className="place-view">
-                    <div className="place-img">
-                        <img src="http://g.wieszjak.pl/p/_wspolne/pliki_infornext/8000/8016.jpg" />
-                    </div>
-                    <div>
-                        <p>asdf asdf asdf asdfd asf asdf asdfdsa</p>
-                        <p>asdf asdf asdf asdfd asf asdf asdfdsa asdfadsf sdafsz dfxc</p>
+            <div className="pure-g">
+                <div className="pure-u-1-5 col"></div>
+                <div className="pure-u-2-5 col">
+                    <div className="place-view">
+                        <div className="place-img">
+                            <img src="http://g.wieszjak.pl/p/_wspolne/pliki_infornext/8000/8016.jpg" />
+                        </div>
+                        <div className="place-description">
+                            <p>asdf asdf asdf asdfd asf asdf asdfdsa</p>
+                            <p>asdf asdf asdf asdfd asf asdf asdfdsa asdfadsf sdafsz dfxc</p>
+                        </div>
                     </div>
                 </div>
+                <div className="pure-u-1-5 col map">
+                    <div className="place-likes">
+                        <div>+5</div>
+                        <div>-75</div>
+                    </div>
+                </div>
+                <div className="pure-u-1-5 col"></div>
             </div>
         );
     }
